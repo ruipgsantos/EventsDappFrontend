65d05ae0afc8d1cf855decd7a21c1372d374343c
Rui Santos <ruipgs@gmail.com>
2022-11-24 12:27:15 +0000

    login logic

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Space from "./views/Space";
 import EditUser from "./views/EditUser";
 import MetamaskButton from "./components/MetamaskButton";
 import { useCallback } from "react";
-import Cookies from "js-cookie";
 import useDappSession from "./hooks/DappSession";
 
 function App() {
@@ -17,48 +16,27 @@ function App() {
   }, [navigate]);
 
   const [
-    { userAddress, isWeb3Available, web3Object, isLoading, isLoggedIn },
+    { userAddress, isWeb3Available, isLoading, isLoggedIn },
     { login, logout },
   ] = useDappSession({ onLogout });
 
-  // useEffect(() => {
-  //   if (!isWeb3Available) return;
-  //   console.log(`user address changed: ${userAddress}`);
-
-  //   const signMessage = async () => {
-  //     try {
-  //       const signedMessage = await window.ethereum.request({
-  //         method: "personal_sign",
-  //         params: [userAddress, "nonce"],
-  //       });
-
-  //       console.log(signedMessage);
-  //     } catch (e) {
-  //       console.error(e);
-  //     }
-  //   };
-
-  //   if (userAddress) {
-  //     signMessage();
-  //   }
-  // }, [isWeb3Available, userAddress, web3Obj]);
   return (
     <div className="App">
       <Nav className="header">
         <h1 className="title">
           <Link to="/events">Events</Link>
         </h1>
-        {userAddress && (
+        {isLoggedIn && (
           <Nav.Item>
             <Link to="/edituser">Edit Profile</Link>
           </Nav.Item>
         )}
-        {userAddress && (
+        {isLoggedIn && (
           <Nav.Item>
             <Link to="/myspace">Edit Space</Link>
           </Nav.Item>
         )}
-        <div className="userAddress">{userAddress}</div>
+        {isLoggedIn && <div className="userAddress">{userAddress}</div>}
         <Nav.Item>
           <MetamaskButton
             isWeb3Available={isWeb3Available}
--- src/hooks/DappSession.js
+++ src/hooks/DappSession.js
@@ -1,6 +1,18 @@
 import { useCallback, useEffect, useState } from "react";
 import Web3 from "web3";
+import { auth } from "../server/server";
+import Cookies from "js-cookie";
+import useCookie from "./Cookie";
 
+/**
+ * Hook designed to handle entire session of a dapp
+ *
+ * @param initUserAddress
+ * Initial user public key address
+ * @param  onLogout
+ * Optional method to execute when logout is triggered
+ * @returns
+ */
 const useDappSession = ({ initUserAddress, onLogout }) => {
   const [userAddress, setUserAddress] = useState(initUserAddress);
   const [isWeb3Available, setWeb3Available] = useState(false);
@@ -8,6 +20,13 @@ const useDappSession = ({ initUserAddress, onLogout }) => {
   const [web3Object, setWeb3Obj] = useState(undefined);
   const [isLoading, setIsLoading] = useState(false);
 
+  const [isAuthenticated, setIsAuthenticated] = useCookie({
+    key: "isAuthenticated",
+  });
+
+  /**
+   * Method to handle logout. If existing, it will also execute the provided {@link onLogout} method
+   */
   const doLogout = useCallback(() => {
     setUserAddress(undefined);
     setIsLoggedIn(false);
@@ -15,7 +34,13 @@ const useDappSession = ({ initUserAddress, onLogout }) => {
     if (onLogout) {
       onLogout();
     }
+
+    //TODO: request server to delete session
   }, [onLogout]);
+
+  useEffect(() => {
+    setIsLoggedIn(!!isAuthenticated);
+  }, [isAuthenticated]);
 
   useEffect(() => {
     if (window.ethereum) {
@@ -34,24 +59,79 @@ const useDappSession = ({ initUserAddress, onLogout }) => {
     }
   }, [doLogout]);
 
-  const startWeb3Login = async () => {
+  /**
+   * Requests connection for user's Metamask wallet
+   *
+   * @returns
+   */
+  const doWeb3Login = async () => {
     if (isWeb3Available) {
-      try {
-        const addArr = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        setUserAddress(addArr[0]);
-        setIsLoading(false);
-        setIsLoggedIn(true);
-      } catch (err) {
-        setIsLoading(false);
-        console.error(`User denied account access: ${err}`);
-      }
+      const addArr = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setUserAddress(addArr[0]);
+
+      return addArr[0];
     }
   };
 
-  const login = () => {
-    startWeb3Login();
+  /**
+   * Given a user wallet address, executes the challenge-response flow to authenticate with server
+   *  - request nonce from server with public key
+   *  - sign nonce with private key
+   *  - request login with signed message and public key
+   *
+   * @param {*} userAddr
+   */
+  const doServerLogin = async (userAddr) => {
+    //request nonce from server
+    const nonce = await auth.requestNonce(userAddr);
+
+    //request sign from metamask user
+    const params = [
+      {
+        type: "string",
+        name: "nonce",
+        value: nonce,
+      },
+    ];
+
+    let signedMessage = await window.ethereum.request({
+      method: "eth_signTypedData",
+      params: [params, userAddr],
+      from: userAddr,
+    });
+
+    //send back to server and await response
+    await auth.sendSignedMessage({
+      signedmsg: signedMessage,
+      pubkey: userAddr,
+    });
+  };
+
+  /**
+   * Login flow. It will connect user Metamask wallet and request authentication from server
+   */
+  const login = async () => {
+    try {
+      setIsLoading(true);
+
+      //web3
+      const userAddr = await doWeb3Login();
+
+      //server
+      await doServerLogin(userAddr);
+
+      setIsAuthenticated(Cookies.get("isAuthenticated"));
+      setIsLoggedIn(true);
+    } catch (err) {
+      console.error(`Could not login`);
+      console.error(err);
+      setIsLoggedIn(false);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const logout = doLogout;
